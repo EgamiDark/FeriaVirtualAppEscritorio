01ed6ba3c070ac4f32082534a88594deabb03d15
cortesnicolas <cortesnicolas006@gmail.com>
2021-09-10 21:31:05 -0300

    Primer Commit - Diseño

---
+++ FeriaVirtualApp/ViewModels/MainViewModel.cs
@@ -0,0 +1,60 @@
+﻿using FeriaVirtualApp.Core;
+
+namespace FeriaVirtualApp.ViewModels
+{
+    internal class MainViewModel : ObservableObject
+    {
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand VentasViewCommand { get; set; }
+        public RelayCommand ResumenVentasViewCommand { get; set; }
+        public RelayCommand TransportistaViewCommand { get; set; }
+
+        public HomeViewModel HomeVM { get; set; }
+        public VentasViewModel VentasVM { get; set; }
+        public ResumenVentasViewModel RVVM { get; set; }
+        public TransportistaViewModel TransportistaVM { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get => _currentView;
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        public MainViewModel()
+        {
+            HomeVM = new HomeViewModel();
+            VentasVM = new VentasViewModel();
+            RVVM = new ResumenVentasViewModel();
+            TransportistaVM = new TransportistaViewModel();
+
+            CurrentView = HomeVM;
+
+            HomeViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = HomeVM;
+            });
+
+            VentasViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = VentasVM;
+            });
+
+            ResumenVentasViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = RVVM;
+            });
+
+            TransportistaViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = TransportistaVM;
+            });
+        }
+    }
+}
---
+++ FeriaVirtualApp/Views/VentasView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace FeriaVirtualApp.Views
+{
+    /// <summary>
+    /// Interaction logic for VentasView.xaml
+    /// </summary>
+    public partial class VentasView : UserControl
+    {
+        public VentasView()
+        {
+            InitializeComponent();
+        }
+    }
+}
